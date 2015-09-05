090a8c0936e0579ac27b5082335870ac5064b2c1
Jiawei Zhang <jiaweizhang95@hotmail.com>
2015-09-05 19:56:09 -0400

    Working Slim request parser.

--- api/include/DbConnect.php
+++ api/include/DbConnect.php
@@ -21,8 +21,12 @@ class DbConnect {
         include_once dirname(__FILE__) . '/Config.php';
 
         // Connecting to mysql database
-        $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
+        $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, null, DB_UNIX);
+        /*try {
+        	$this->conn = new pdo(DB_PDO, DB_USERNAME, DB_PASSWORD);
+        } catch (PDOException $e) {
+        	echo "Error!: " . $e->getMessage() ."<br/>";
+        }*/
         // Check for database connection error
         if (mysqli_connect_errno()) {
             echo "Failed to connect to MySQL: " . mysqli_connect_error();
